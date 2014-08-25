62f944c4ab7b75bf000715b6947998fbaf30682f
skywalkerd <iseme.beto@gmail.com>
2014-08-25 09:00:48 -0300

    Added empty README and the whole code.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 /**
  * Created by root on 25/08/14.
  */
+
+
+// If given only a obj, the function returns true if object obj is defined and not null and false otherwise
+//     something like pseudo: return = obj is defined
+// If given an opt value, the function returns obj itself if object is defined and not null and returns opt otherwise
+//     something like pseudo: return = obj, if its defined, or opt (obj | opt, but avoiding javascript default behavior)
+
+module.exports = function(obj, opt){
+    if(arguments.length > 1)
+        return obj !== undefined && obj != null ? obj : opt;
+    return obj !== undefined && obj != null;
+};
